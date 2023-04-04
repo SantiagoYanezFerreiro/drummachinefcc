5c44961e1d40b2a8197eca0243f30beaedd29479
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-04-04 22:07:25 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.86ed4dce4adc626aa6aa16490453132d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb3c1d73aab46ae8352f",
+    "url": "/drummachinefcc/static/css/main.d4fba2a9.chunk.css"
+  },
+  {
+    "revision": "fb3c1d73aab46ae8352f",
+    "url": "/drummachinefcc/static/js/main.fb3c1d73.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/drummachinefcc/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/drummachinefcc/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "1d402d67f4cf0fb660d8",
+    "url": "/drummachinefcc/static/js/runtime~main.1d402d67.js"
+  },
+  {
+    "revision": "9c5d399b218ebb5f858d3386e9dd6134",
+    "url": "/drummachinefcc/index.html"
+  }
+];
